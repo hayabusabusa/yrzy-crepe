6c8af86804ada4dc783562580a9d336a3b275545
hayabusabusa <hayabusabusaSY@gmail.com>
2022-02-24 17:37:52 +0900

    fix: サンプルコードを削除

---
+++ app/lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Scaffold(
+        body: Center(
+          child: Text("app"),
+        ),
+      ),
+    );
+  }
+}
