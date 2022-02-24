53047acd934e873a61b276af47a9cea4c42434de
hayabusabusa <hayabusabusaSY@gmail.com>
2022-02-25 00:11:02 +0900

    ui: ネットワークイメージ用の Widget を修正

--- packages/crepe_ui/lib/crp_network_image.dart
+++ packages/crepe_ui/lib/crp_network_image.dart
@@ -4,9 +4,15 @@ import 'package:cached_network_image/cached_network_image.dart';
 
 class CRPNetworkImage extends StatelessWidget {
   final String imageURL;
+  final double? width;
+  final double? height;
+  final BoxFit? fit;
 
   const CRPNetworkImage({
     Key? key,
+    this.width,
+    this.height,
+    this.fit,
     required this.imageURL,
   }) : super(key: key);
 
@@ -14,6 +20,9 @@ class CRPNetworkImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return CachedNetworkImage(
       imageUrl: imageURL,
+      width: width,
+      height: height,
+      fit: fit,
     );
   }
 }
