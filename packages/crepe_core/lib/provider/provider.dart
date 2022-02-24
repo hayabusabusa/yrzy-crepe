b4d01dff0ea39b16c2a405b35d8fa0862df648b6
hayabusabusa <hayabusabusaSY@gmail.com>
2022-02-25 00:10:36 +0900

    fix: core パッケージの barrel ファイルを修正

--- packages/crepe_core/lib/provider/provider.dart
+++ packages/crepe_core/lib/provider/provider.dart
@@ -2,3 +2,4 @@ export './crp_auth_provider.dart';
 export './crp_collection_refrencable.dart';
 export './crp_firestore_provider.dart';
 export './crp_query_referencable.dart';
+export './crp_referencable.dart';
