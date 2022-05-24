1c6ec9af7f4c8f53f7f0593142a3f48224e71ab4
hayabusabusa <hayabusabusaSY@gmail.com>
2022-05-25 01:27:48 +0900

    ui: ビューワー画面にフッターを追加

--- app/lib/screens/viewer/viewer_screen.dart
+++ app/lib/screens/viewer/viewer_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:crepe_ui/crepe_ui.dart';
-
 import 'package:crepe_core/crepe_core.dart';
 
 // MARK: - Screen
@@ -42,7 +41,7 @@ class _Body extends StatefulWidget {
 class _BodyState extends State<_Body> {
   final PageController _controller = PageController();
 
-  bool _isTapped = false;
+  bool _isTapped = true;
 
   @override
   Widget build(BuildContext context) {
@@ -52,13 +51,142 @@ class _BodyState extends State<_Body> {
           _isTapped = !_isTapped;
         });
       },
-      child: PageView.builder(
-        itemCount: widget.imageURLs.length,
-        itemBuilder: (_, index) {
-          return CRPNetworkImage(imageURL: widget.imageURLs[index]);
-        },
-        reverse: true,
-        controller: _controller,
+      child: SafeArea(
+        child: Stack(
+          children: [
+            // PageView
+            PageView.builder(
+              itemCount: widget.imageURLs.length,
+              itemBuilder: (_, index) {
+                return CRPNetworkImage(imageURL: widget.imageURLs[index]);
+              },
+              reverse: true,
+              controller: _controller,
+            ),
+            // Footer
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: AnimatedOpacity(
+                opacity: _isTapped ? 0 : 1,
+                duration: const Duration(milliseconds: 200),
+                child: AbsorbPointer(
+                  absorbing: _isTapped,
+                  child: _Footer(
+                    min: 0, 
+                    max: widget.imageURLs.length - 1, 
+                    onChanged: (value) {
+                      _controller.jumpToPage(value);
+                    },
+                    controller: _controller,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// MARK: - Footer
+
+class _Footer extends StatefulWidget {
+  final int max;
+  final int min;
+  final ValueChanged<int> onChanged;
+  final PageController? controller;
+
+  const _Footer({ 
+    Key? key,
+    required this.min,
+    required this.max,
+    required this.onChanged,
+    this.controller,
+  }) : super(key: key);
+
+  @override
+  State<_Footer> createState() => _FooterState();
+}
+
+class _FooterState extends State<_Footer> {
+  double _selectedValue = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _selectedValue = widget.max.toDouble();
+
+    widget.controller?.addListener(() { 
+      final page = widget.controller?.page ?? 0;
+      final selectedValue = widget.max - page;
+
+      if (_selectedValue != selectedValue) {
+        setState(() {
+          _selectedValue = selectedValue;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Theme.of(context).scaffoldBackgroundColor,
+      child: Column(
+        children: [
+          // Divider
+          Container(
+            height: 1,
+            color: Theme.of(context).dividerColor,
+          ),
+          // Slider
+          Slider(
+            min: widget.min.toDouble(),
+            max: widget.max.toDouble(),
+            value: _selectedValue,
+            label: (widget.max - _selectedValue + 1).round().toString(),
+            divisions: widget.max,
+            onChanged: (value) {
+              final invertedValue = widget.max - value;
+              widget.onChanged(invertedValue.round());
+          
+              setState(() {
+                _selectedValue = value;
+              });
+            }
+          ),
+          // Buttons
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    onPressed: () {}, 
+                    child: const Text("お気に入り")
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      primary: Theme.of(context).errorColor,
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    }, 
+                    child: const Text("閉じる")
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
