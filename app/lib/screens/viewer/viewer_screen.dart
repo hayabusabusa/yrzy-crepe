a0f504bcadaafde05f40863cf18eab502990b816
hayabusabusa <hayabusabusaSY@gmail.com>
2022-05-25 22:32:31 +0900

    feat: お気に入りに追加の機能を実装

--- app/lib/screens/viewer/viewer_screen.dart
+++ app/lib/screens/viewer/viewer_screen.dart
@@ -19,7 +19,7 @@ class ViewerScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(book.title),
       ),
-      body: _Body(imageURLs: book.imageURLs),
+      body: _Body(book: book),
     );
   }
 }
@@ -27,11 +27,11 @@ class ViewerScreen extends StatelessWidget {
 // MARK: - Body
 
 class _Body extends StatefulWidget {
-  final List<String> imageURLs;
+  final Book book;
 
   const _Body({ 
     Key? key,
-    required this.imageURLs,
+    required this.book,
   }) : super(key: key);
 
   @override
@@ -56,9 +56,9 @@ class _BodyState extends State<_Body> {
           children: [
             // PageView
             PageView.builder(
-              itemCount: widget.imageURLs.length,
+              itemCount: widget.book.imageURLs.length,
               itemBuilder: (_, index) {
-                return CRPNetworkImage(imageURL: widget.imageURLs[index]);
+                return CRPNetworkImage(imageURL: widget.book.imageURLs[index]);
               },
               reverse: true,
               controller: _controller,
@@ -75,7 +75,8 @@ class _BodyState extends State<_Body> {
                   absorbing: _isTapped,
                   child: _Footer(
                     min: 0, 
-                    max: widget.imageURLs.length - 1, 
+                    max: widget.book.imageURLs.length - 1, 
+                    book: widget.book,
                     onChanged: (value) {
                       _controller.jumpToPage(value);
                     },
@@ -96,6 +97,7 @@ class _BodyState extends State<_Body> {
 class _Footer extends StatefulWidget {
   final int max;
   final int min;
+  final Book book;
   final ValueChanged<int> onChanged;
   final PageController? controller;
 
@@ -103,6 +105,7 @@ class _Footer extends StatefulWidget {
     Key? key,
     required this.min,
     required this.max,
+    required this.book,
     required this.onChanged,
     this.controller,
   }) : super(key: key);
@@ -166,10 +169,7 @@ class _FooterState extends State<_Footer> {
               children: [
                 Expanded(
                   flex: 1,
-                  child: TextButton(
-                    onPressed: () {}, 
-                    child: const Text("お気に入り")
-                  ),
+                  child: _FavoriteButton(book: widget.book,),
                 ),
                 Expanded(
                   flex: 1,
@@ -188,6 +188,84 @@ class _FooterState extends State<_Footer> {
           ),
         ],
       ),
+    );
+  }
+}
+
+// MARK: - Favorite Button
+
+class _FavoriteButton extends StatefulWidget {
+  final Book book;
+
+  const _FavoriteButton({ 
+    Key? key,
+    required this.book,
+  }) : super(key: key);
+
+  @override
+  State<_FavoriteButton> createState() => _FavoriteButtonState();
+}
+
+class _FavoriteButtonState extends State<_FavoriteButton> {
+
+  @override
+  Widget build(BuildContext context) {
+    final uid = CRPAuthProvider.instance.uid();
+    if (uid == null) {
+      return const TextButton(
+        onPressed: null, 
+        child: Text("お気に入り"),
+      );
+    }
+
+    final documentReferencable = CRPFavoritesDocumentReferencable(
+      uid: uid, 
+      documentID: widget.book.id
+    );
+
+    return FutureBuilder<bool>(
+      future: CRPFirestoreProvider.instance.isExistsDocument(documentReferencable: documentReferencable),
+      builder: (_, asyncSnapshot) {
+        if (!asyncSnapshot.hasData) {
+          return const CircularProgressIndicator();
+        }
+
+        if (asyncSnapshot.hasError) {
+          return const TextButton(
+            onPressed: null, 
+            child: Text("エラーが発生しました")
+          );
+        }
+
+        final isExists = asyncSnapshot.data ?? false;
+        return isExists 
+          ? TextButton.icon(
+            onPressed: () { 
+              /* お気に入りから削除の処理を入れる. */
+            }, 
+            icon: const Icon(Icons.check_rounded),
+            label: const Text("お気に入り済み"),
+          )
+          : TextButton(
+            onPressed: () async {
+              final favoriteBook = FavoriteBook(
+                id: widget.book.id, 
+                title: widget.book.title, 
+                thumbnailURL: widget.book.thumbnailURL,
+              );
+              
+              try {
+                await CRPFirestoreProvider.instance.setDocument(data: favoriteBook, documentReferencable: documentReferencable);
+              } catch (_) {
+                // 今のところ何もしない.
+              }
+
+              // ボタンの状態を更新するために、Widget を更新する.
+              setState(() {});
+            }, 
+            child: const Text("お気に入り")
+          );
+      },
     );
   }
 }
