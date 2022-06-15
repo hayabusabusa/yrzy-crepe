b445229fc50695da8655716fcfaad76c89311870
hayabusabusa <hayabusabusaSY@gmail.com>
2022-06-15 16:47:01 +0900

    ui: ページネーション用の ListView を追加

--- packages/crepe_ui/lib/crepe_ui.dart
+++ packages/crepe_ui/lib/crepe_ui.dart
@@ -1,4 +1,5 @@
 library crepe_ui;
 
 export './crp_network_image.dart';
+export './crp_pagination_list_view.dart';
 export './crp_theme.dart';
---
+++ packages/crepe_ui/lib/crp_pagination_list_view.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+/// ページネーション用の `ListView`.
+class CRPPaginationListView extends StatefulWidget {
+  /// ページネーションで追加読み込みを行うスクロール領域の閾値.
+  /// 
+  /// デフォルトは `40.0` で設定.
+  final double threshold;
+  final ScrollController scrollController;
+  final int itemCount;
+  final VoidCallback onReachBottom;
+  final Widget Function(BuildContext, int) itemBuilder;
+
+  const CRPPaginationListView({ 
+    Key? key,
+    this.threshold = 40.0,
+    required this.scrollController,
+    required this.itemCount,
+    required this.onReachBottom,
+    required this.itemBuilder,
+  }) : super(key: key);
+
+  @override
+  State<CRPPaginationListView> createState() => _CRPPaginationListViewState();
+}
+
+class _CRPPaginationListViewState extends State<CRPPaginationListView> {
+
+  @override
+  void initState() {
+    super.initState();
+    widget.scrollController.addListener(_listener);
+  }
+
+  @override
+  void dispose() {
+    widget.scrollController.removeListener(_listener);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      controller: widget.scrollController,
+      itemCount: widget.itemCount,
+      itemBuilder: widget.itemBuilder,
+    );
+  }
+
+  /// スクロールのイベント発生時に処理を行うリスナー.
+  void _listener() {
+    final maxScrollExtent = widget.scrollController.position.maxScrollExtent;
+    final position = widget.scrollController.position.pixels;
+
+    // `スクロール領域 - 現在のスクロール位置` で残りのスクロール可能領域を計算する.
+    // 慣性スクロール分を無視するため 0 <= 残りのスクロール領域 <= 閾値 になったら次のページを取得する.
+    final diff = maxScrollExtent - position;
+    final isReachBottom = 0 <= diff && diff <= widget.threshold;
+    
+    if (isReachBottom) {
+      widget.onReachBottom();
+    }
+  }
+}
